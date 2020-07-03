c07adb9f2ef451f59762d45bff11cc04d0f6a661
azakimi123 <aaron.zakimi@gmail.com>
2020-07-03 12:50:17 -0600

    dummy data

--- src/App.js
+++ src/App.js
@@ -5,11 +5,36 @@ import Header from './Components/Header/Header';
 import './App.css';
 
 class App extends Component{
+  constructor(props) {
+    super(props);
+    this.state ={
+      list: [
+        {
+          img:'https://scene7.zumiez.com/is/image/zumiez/pdp_hero/Brixton-Jolt-Black-Trucker-Hat-_290849-front-US.jpg',
+          name: 'Brixton Hat',
+          price: 24.99
+        },
+
+        {
+          img:'https://scene7.zumiez.com/is/image/zumiez/pdp_hero/Thrasher-Godzilla-Red-Hoodie-_323721-front-US.jpg',
+          name: 'Thrasher Hoodie',
+          price: 67.95
+        },
+
+        {
+          img:'https://scene7.zumiez.com/is/image/zumiez/pdp_hero/Primitive-x-Naruto-Ichiraku-Black-T-Shirt-_331191-front-US.jpg',
+          name: 'Primitive Naruto Shirt',
+          price: 33.95
+        }
+      ]
+    }
+  }
   render() {
     return (
       <div>
         <Header/>
-        <Dashboard/>
+        <Dashboard
+        list={this.state.list}/>
         <Form/>
       </div>
     )
