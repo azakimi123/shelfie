c09de46e793a3825f93f41c2293c4dfddbf1113c
azakimi123 <aaron.zakimi@gmail.com>
2020-07-03 15:08:22 -0600

    post handler function added

--- server/controller.js
+++ server/controller.js
@@ -5,5 +5,16 @@ module.exports = {
         db.get_inventory()
         .then(inventory => res.status(200).send(inventory))
         .catch(err => res.status(500).send(err))
+    },
+
+    addProduct: (req, res) => {
+        const {name, price, img} = req.body;
+        const db = req.app.get('db');
+    
+
+
+        db.create_product({name, price, img})
+        .then( () => res.sendStatus(200))
+        .catch(err => res.status(500).send(err));
     }
 }
