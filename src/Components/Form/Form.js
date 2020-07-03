ae6e89c725621868dfbc5bd96c6a13945312fcd1
azakimi123 <aaron.zakimi@gmail.com>
2020-07-03 14:24:53 -0600

    form container styled

--- src/Components/Form/Form.js
+++ src/Components/Form/Form.js
@@ -39,21 +39,29 @@ handleCancelBtn = () => {
         // console.log(this.state.price)
         return (
             <div>
-                <img className='picture-box' src={this.state.imgurl} alt='product'/>
-                <p>Img URL:</p>
-                <input
-                    value={this.state.imgurl}
-                    onChange={ e => this.handleImg(e.target.value)}/>
-                <p>Name:</p>
-                <input
-                    value={this.state.name}
-                    onChange={ e => this.handleName(e.target.value)}/>
-                <p>Price:</p>
-                <input
-                    value={this.state.price}
-                    onChange={ e => this.handlePrice(e.target.value)}/>
-                <button onClick={this.handleCancelBtn}>Cancel</button>
-                <button>Add to Inventory</button>
+                <section className='form-container'>
+                    <img className='picture-box' src={this.state.imgurl} alt='product'/>
+                    <p>Img URL:</p>
+                    <input
+                        value={this.state.imgurl}
+                        onChange={ e => this.handleImg(e.target.value)}/>
+                    <p>Name:</p>
+                    <input
+                        value={this.state.name}
+                        onChange={ e => this.handleName(e.target.value)}/>
+                    <p>Price:</p>
+                    <input
+                        value={this.state.price}
+                        onChange={ e => this.handlePrice(e.target.value)}/>
+                    <div className='button-container'>
+                        <button 
+                            className='cancel-btn'
+                            onClick={this.handleCancelBtn}>
+                            Cancel
+                        </button>
+                        <button className='add-btn'>Add to Inventory</button>
+                    </div>
+                </section>
             </div>
         )
     }
