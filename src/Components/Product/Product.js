0bfd1a4ffbb1e8460d5b0e7b4df02d74e132b2e3
azakimi123 <aaron.zakimi@gmail.com>
2020-07-03 12:51:36 -0600

    img, name, price displays from props

--- src/Components/Product/Product.js
+++ src/Components/Product/Product.js
@@ -1,10 +1,16 @@
 import React, {Component} from 'react';
+import './Product.css';
 
 class Product extends Component {
+    
     render() {
+        console.log(this.props.product)
+        const product = this.props.product;
         return (
             <div>
-                Product
+                <img className='image'src={product.img}/>
+                <p>Name: {product.name}</p>
+                <p>Price: ${product.price}</p>
             </div>
         )
     }
