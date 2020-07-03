a1596eb52dbbd2d3415194cd8e3a877e8ea3bc52
azakimi123 <aaron.zakimi@gmail.com>
2020-07-03 13:48:08 -0600

    axios.get list

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import axios from 'axios';
 import Dashboard from './Components/Dashboard/Dashboard';
 import Form from './Components/Form/Form';
 import Header from './Components/Header/Header';
@@ -8,27 +9,18 @@ class App extends Component{
   constructor(props) {
     super(props);
     this.state ={
-      list: [
-        {
-          img:'https://scene7.zumiez.com/is/image/zumiez/pdp_hero/Brixton-Jolt-Black-Trucker-Hat-_290849-front-US.jpg',
-          name: 'Brixton Hat',
-          price: 24.99
-        },
-
-        {
-          img:'https://scene7.zumiez.com/is/image/zumiez/pdp_hero/Thrasher-Godzilla-Red-Hoodie-_323721-front-US.jpg',
-          name: 'Thrasher Hoodie',
-          price: 67.95
-        },
-
-        {
-          img:'https://scene7.zumiez.com/is/image/zumiez/pdp_hero/Primitive-x-Naruto-Ichiraku-Black-T-Shirt-_331191-front-US.jpg',
-          name: 'Primitive Naruto Shirt',
-          price: 33.95
-        }
-      ]
+      list: []
     }
   }
+
+componentDidMount = () => {
+  axios.get('/api/inventory')
+  .then(res => {
+    this.setState({list: res.data})
+  })
+  .catch( err => console.log(err));
+}
+
   render() {
     return (
       <div>
