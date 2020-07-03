8e4fa3dd4b51f01bff4468d3900655eb7c49e1fc
azakimi123 <aaron.zakimi@gmail.com>
2020-07-03 12:50:44 -0600

    mapped list from app.js and passed props

--- src/Components/Dashboard/Dashboard.js
+++ src/Components/Dashboard/Dashboard.js
@@ -2,11 +2,19 @@ import React, {Component} from 'react';
 import Product from '../Product/Product';
 
 class Dashboard extends Component {
+    constructor(props) {
+        super(props);
+    }
     render() {
+        const list = this.props.list;
+        // console.log(list)
+        const mappedList = list.map((product, index) => (
+            <Product key={index} product={product}/>
+        ))
         return (
             <div>
                 Dashboard
-                <Product/>
+                {mappedList}
             </div>
         )
     }
